5821eaf5f5ca4009a6bb4f946cb254063d422f79
tendy <pattileilohyterry@gmail.com>
2018-10-29 11:32:38 +0700

    tes

---
+++ typedata.py
@@ -0,0 +1,35 @@
+#tipe data boolean
+#print(true)
+
+#tipe data string
+print('Belajar tipe data')
+print("dalam python")
+
+#tipe data integer
+print(1997)
+
+#tipe data float
+print(3.65)
+
+#tipe data hexadecimal
+#print(9a)
+
+#tipe data complex
+print(5j)
+
+#tipe data list
+print([1,2,3,4,5])
+print(["satu","dua","tiga"])
+
+#tipe data tuple
+print((1,2,3,4,5))
+print(("satu","dua","tiga"))
+
+#tipe data dictionary
+print({"nama":"rvld", 'umur':21})
+#tipe data dictionary dimasukan ke dalam variable biodata
+
+biodata = {"nama":"leony", 'umur':3} #inisialisai variable
+print(biodata) #menampilkan variable biodata yang berisi tipe data dictionary
+
+type(biodata) #menampilkan jenis tipe data. akan tampil <class 'dict'> yang berarti dict adalah tipe data dictionary

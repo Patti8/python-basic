e5be6560c5aa54b02170caab2d6a29ec9e8ac96c
tendy <pattileilohyterry@gmail.com>
2018-11-09 06:24:53 +0700

    loop

---
+++ kondisi.py
@@ -0,0 +1,34 @@
+print("=========KONDISI IF")
+#KONDISI IF
+#kondisi yang akan dieksekusi oleh program jika nilai bernilai benar TRUE
+
+nilai = 9
+if(nilai > 7):
+    print("anda lulus")
+
+print("===========KONDISI ELSE")
+#KONDISI ELSE
+if(nilai > 10):
+    print("anda lulus")
+else:
+    print("anda tidak lulus")
+
+
+print("===============KONDISI ELIF")
+#KONDISI ELIF
+
+hari_ini = "Minggu"
+if(hari_ini == "Senin"):
+    print("saya akan kuliah")
+elif(hari_ini == "selsasa"):
+    print("Saya akan kuliah")
+elif(hari_ini == "Rabu"):
+    print("Saya akan kuliah")
+elif(hari_ini == "kamis"):
+    print("saya akan kuliah")
+elif(hari_ini == "Jumat"):
+    print("saya akan kuliah")
+elif(hari_ini == "Sabtu"):
+    print("saya libur")
+elif(hari_ini == "Minggu"):
+    print("saya libur")
---
+++ loop.py
@@ -0,0 +1,13 @@
+#Perulangan dibagi 3:
+#While Loop
+#For Loop
+#Nested Loop
+
+#Example WHILE LOOP
+count = 0
+while (count < 9):
+    print ('The count is:', count)
+    count = count - 1
+
+print ("Good bye!")
+
